bf931370b3098e38d38428710cb764666a283c17
Robertarezza <roberta.rezza@gmail.com>
2024-05-17 11:40:40 +0200

    fix scroll

--- src/router.js
+++ src/router.js
@@ -5,30 +5,40 @@ import About from './pages/About.vue';
 import Contacts from './pages/Contacts.vue';
 import NotFound from './pages/NotFound.vue';
 
-const router = createRouter ({
-history: createWebHistory(),
-routes: [
-    {
-        path: "/",
-        name: "home",
-        component: HomePage,
-    },
-    {
-        path: "/contacts",
-        name: "contacts",
-        component: Contacts,
-    },
-    {
-        path: "/about",
-        name: "about",
-        component: About,
-    },
-    {
-        path: "/not-found",
-        name: "notfound",
-        component: NotFound,
-    },
-]
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: HomePage,
+        },
+        {
+            path: "/contacts",
+            name: "contacts",
+            component: Contacts,
+        },
+        {
+            path: "/about",
+            name: "about",
+            component: About,
+        },
+        {
+            path: "/not-found",
+            name: "notfound",
+            component: NotFound,
+        },
+    ],
+
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return {
+                top: 0
+            };
+        }
+    }
 })
 
 export { router }
